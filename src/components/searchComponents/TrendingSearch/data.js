0cf9d7f204d337145570e982d20185b604674e9f
xuannhutzz <xuannhutzz@gmail.com>
2022-08-28 06:08:17 +0700

    update: data fake trending seach

--- src/components/searchComponents/TrendingSearch/data.js
+++ src/components/searchComponents/TrendingSearch/data.js
@@ -1,62 +1,62 @@
 export const trendingSearchData = [
-    {
-        id: Math.random() * 1000000,
-        text: "shoes menxxxxxxxxxxxxxxx ",
-        count: Math.floor(Math.random() * 10000),
-        url: "62f7e2255e6ac0ae18128ed5"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes women",
-        count: Math.floor(Math.random() * 10000),
-        url: "62b462ceb2eb2ab245d33327"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes children",
-        count: Math.floor(Math.random() * 10000),
-        url: "62ab2799dd3e2191fb5dae67"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes children",
-        count: Math.floor(Math.random() * 10000),
-        url: "62aad2fe89e352a5b3bd07ac"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes children",
-        count: Math.floor(Math.random() * 10000),
-        url: "62aad2c589e352a5b3bd07a4"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes children",
-        count: Math.floor(Math.random() * 10000),
-        url: "62aad23689e352a5b3bd0772"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes children",
-        count: Math.floor(Math.random() * 10000),
-        url: "62aad20d89e352a5b3bd076a"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes children",
-        count: Math.floor(Math.random() * 10000),
-        url: "62aacf4a89e352a5b3bd06f4"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes children",
-        count: Math.floor(Math.random() * 10000),
-        url: "62aacf0a89e352a5b3bd06ec"
-    },
-    {
-        id: Math.random() * 1000000,
-        text: "shoes children",
-        count: Math.floor(Math.random() * 10000),
-        url: "62aa1a8c67d4d9cfcc7e706f"
-    }
+  {
+    id: Math.random() * 1000000,
+    text: "shoes men ",
+    count: Math.floor(Math.random() * 10000),
+    url: "62f7e2255e6ac0ae18128ed5"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "shoes women",
+    count: Math.floor(Math.random() * 10000),
+    url: "62b462ceb2eb2ab245d33327"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "children",
+    count: Math.floor(Math.random() * 10000),
+    url: "62ab2799dd3e2191fb5dae67"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "men",
+    count: Math.floor(Math.random() * 10000),
+    url: "62aad2fe89e352a5b3bd07ac"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "sport",
+    count: Math.floor(Math.random() * 10000),
+    url: "62aad2c589e352a5b3bd07a4"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "snake",
+    count: Math.floor(Math.random() * 10000),
+    url: "62aad23689e352a5b3bd0772"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "nike",
+    count: Math.floor(Math.random() * 10000),
+    url: "62aad20d89e352a5b3bd076a"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "shoes children",
+    count: Math.floor(Math.random() * 10000),
+    url: "62aacf4a89e352a5b3bd06f4"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "western",
+    count: Math.floor(Math.random() * 10000),
+    url: "62aacf0a89e352a5b3bd06ec"
+  },
+  {
+    id: Math.random() * 1000000,
+    text: "shoes",
+    count: Math.floor(Math.random() * 10000),
+    url: "62aa1a8c67d4d9cfcc7e706f"
+  }
 ];
