ffa3e56d8e979239975b4f97f01336d90ac35b7e
xuannhutzz <xuannhutzz@gmail.com>
2023-01-31 22:39:53 +0700

    style: done update slide and responsive

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -20,16 +20,38 @@ const slideImages = [
 const Slideshow = () => {
   return (
     <div className="slide-container">
-      <Fade>
-        {slideImages.map((slideImage, index) => (
-          <div className="each-slide" key={index}>
-            {/* <div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
-            </div> */}
-            <img src={slideImage.url} alt={slideImage.caption} />
-          </div>
-        ))}
-      </Fade>
+      <div className="slide-container__left">
+        <Fade>
+          {slideImages.map((slideImage, index) => (
+            <div className="each-slide" key={index}>
+              {/* <div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+              </div> */}
+              <img src={slideImage.url} alt={slideImage.caption} />
+            </div>
+          ))}
+        </Fade>
+      </div>
+      <div className="slide-container__right">
+        <a href="$">
+          <div
+            style={{
+              backgroundImage: "url('./images/img-size-l/sr-01.png')",
+              backgroundSize: "cover",
+              height: "49%"
+            }}
+          ></div>
+        </a>
+        <a href="$">
+          <div
+            style={{
+              backgroundImage: "url('./images/img-size-l/m_bn_2_2.jpg')",
+              backgroundSize: "cover",
+              height: "49%"
+            }}
+          ></div>
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 export default Slideshow;
